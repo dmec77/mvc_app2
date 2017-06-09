395474148d97c361ee54e7efbbaf9844aed7145a
Diane Carlton <dimargcarl@gmail.com>
2017-06-09 10:38:43 -0400

    adding ejs

--- controllers/recipes/controller.js
+++ controllers/recipes/controller.js
@@ -6,9 +6,9 @@ controller.index = (req, res) => {
   Recipe.findAll()
   .then((recipe_data) => {
     res.render('recipes/index', {
-    //????*****????? this is saying that based on the data we are pulling above, we will use "beers" to represent that data.
+    //????*****????? this is saying that based on the data we are pulling above, we will use "recipe_data" to represent that data.
 
-    //the name before the colon should match what will be replaced on th eejs file, and the "recipe_data" is what data was gathered to be displayed(so use "recipe."" in the place you want the info to change, and based on the FX above it will change to what ever "brecipe_data" Fx comes up with)
+    //the name before the colon should match what will be replaced on th ejs file, and the "recipe_data" is what data was gathered to be displayed(so use "recipe."" in the place you want the info to change, and based on the FX above it will change to what ever "recipe_data" Fx comes up with)
       recipe: recipe_data
     });
   })
---
+++ public/app.js
@@ -0,0 +1,12 @@
+	"use strict";
+
+	$(document).ready(function() {
+
+});
+
+$("#cat").on("click", function {
+  window.location.href = "/recipes/index.ejs"
+    // if "Appetizers" clicked
+    //   then display
+
+});
